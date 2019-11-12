ed3e5576924a334dc9fcabee5dbeec1b7e330a54
pipu11qiao <18854558196@163.com>
2019-11-12 16:07:22 +0800

    3

--- leetcode/1/3_longest-substring-without-repeating-characters.js
+++ leetcode/1/3_longest-substring-without-repeating-characters.js
@@ -3,42 +3,21 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    if (s === '') {
-        return 0
-    }
-    let max = 0;
-    let subI = 0;
-    let len = 1;
+    let len = s.length;
+    let res = 0;
     let strMap = {};
-    strMap[s[0]] = 0;
-    for (let i = 0; i < s.length; i++) {
-        for (; subI < s.length; subI++) {
-            if (subI === i) {
-                continue;
-            }
-            if (strMap[s[subI]] === undefined) { // 没有
-                len++;
-                strMap[s[subI]] = subI;
-            } else { // 有重复
-                if (len > max) {
-                    max = len;
-                }
-                len = len - (subI - strMap[s[subI]]) +1;
-                strMap = {};
-                i = subI;
-                strMap[s[subI]] = subI;
-            }
+    for (let j = 0, i = 0; j < len; j++) {
+        if (strMap[s[j]] !== undefined) {
+            i = Math.max(strMap[s[j]], i);
         }
-        if (len > max) {
-            max = len;
-        }
+        res = Math.max(res, j - i + 1);
+        strMap[s[j]] = j + 1;
     }
-    console.log(strMap);
-    return max;
+    return res;
 };
 
 // test
-const str = "aabac" ;
+const str = "aabac";
 
 console.log(lengthOfLongestSubstring(str));
 
