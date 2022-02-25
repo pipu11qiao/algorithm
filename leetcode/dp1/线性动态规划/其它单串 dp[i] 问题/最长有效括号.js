4997e662ec56bc2d1a204c6d7a5e6d5d6903b7e7
王勇 <wangyong16@xiaomi.com>
2022-02-25 19:00:30 +0800

    feat: update code

--- leetcode/dp1/线性动态规划/其它单串 dp[i] 问题/最长有效括号.js
+++ leetcode/dp1/线性动态规划/其它单串 dp[i] 问题/最长有效括号.js
@@ -2,13 +2,61 @@
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function (s) {
+var longestValidParentheses = function (str) {
+  let res = 0;;
+  let len = str.length;
+  let s = -1;
+  let lastS = -1;
+  let e = 0;
+  let c = 0;
+  for (let i = 0; i < len; i++) {
 
+    if (str[i] === '(') {
+      c++
+      if (s === -1) {
+        s = i;
+        e = i;
+      }
+      if (lastS === -1) {
+        lastS = i;
+      }
+
+
+    } else {
+      if (c > 0) {
+        e = i
+      }
+      c--
+      if (c === 0 && s > -1) {
+        lastS = -1;
+        const count = e - s + 1 - c
+        if (count > res) {
+          res = count
+        }
+      }
+      if (c > 0 && i === len - 1) {
+        const count = e - lastS + 1 - c
+        if (count > res) {
+          res = count
+        }
+      }
+      if (c < 0) {
+        s = -1
+        e = -1
+        c = 0
+      }
+    }
+  }
+  return res
 };
 function test() {
   let fun = longestValidParentheses;
   let params = [
-    [1, 1, 1, 0]
+    // "(()"
+    // ")()())"
+    // ''
+    // ")()()()(()"
+    ")(((((()())()()))()(()))("
   ];
 
   const res = fun.apply(null, params)
