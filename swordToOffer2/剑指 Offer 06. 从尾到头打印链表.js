4257e2481c417a480ae1def7ca1ffe2666b6e1ad
王勇 <wangyong16@xiaomi.com>
2022-03-17 12:52:45 +0800

    feat: update

---
+++ swordToOffer2/剑指 Offer 06. 从尾到头打印链表.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+var reversePrint = function (head) {
+
+};
+
+function test() {
+  let fun = reversePrint;
+  let params = [
+    [1, 1, 1, 0]
+  ];
+
+
+  const res = fun.apply(null, params)
+  console.log(`res`, res);
+}
+test()
