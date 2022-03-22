11f253676223b237de40134f261ac806444abb76
wangyong <wangyong16@xiaomi.com>
2022-03-22 09:31:56 +0800

    feat: update

--- swordToOffer2/剑指 Offer 36. 二叉搜索树与双向链表.js
+++ swordToOffer2/剑指 Offer 36. 二叉搜索树与双向链表.js
@@ -10,18 +10,53 @@
  * @param {Node} root
  * @return {Node}
  */
-var treeToDoublyList = function (root) {
-
+function isLeaf(node) {
+  return !node.left && !node.right
+}
+function converse(root) {
+  const { left, right, val } = root;
+  let head = null;
+  let tail = null;
+  root.left = null;
+  root.rigt = null;
+  if (left) {
+    if (isLeaf(left)) {
+      head = left;
+      left.right = root;
+      root.left = left
+    } else {
+      const { head: leftHead, tail: leftTail } = converse(left);
+      head = leftHead
+      leftTail.right = root;
+      root.left = leftTail
+    }
+    tail = root
+  }
+  if (!head) {
+    head = root;
+    tail = root;
+  }
+  if (right) {
+    if (isLeaf(right)) {
+      tail = right
+      right.left = root;
+      root.right = right;
+    } else {
+      const { head: rightHead, tail: rightTail } = converse(right);
+      rightHead.left = tail;
+      tail.right = rightHead
+      tail = rightTail
+    }
+  }
+  return { head, tail }
 };
 
-function test() {
-  let fun = treeToDoublyList;
-  let params = [
-    [1, 1, 1, 0]
-  ];
-
-
-  const res = fun.apply(null, params)
-  console.log(`res`, res);
+var treeToDoublyList = function (root) {
+  if (!root) {
+    return null;
+  }
+  const { head, tail } = converse(root);
+  head.left = tail;
+  tail.right = head;
+  return head
 }
-test()
