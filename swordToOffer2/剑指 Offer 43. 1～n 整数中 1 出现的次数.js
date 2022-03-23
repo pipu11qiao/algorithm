94cff7fd72ba3e85b8a605b91510e59cabced544
王勇 <wangyong16@xiaomi.com>
2022-03-23 19:52:15 +0800

    feat: update

--- swordToOffer2/剑指 Offer 43. 1～n 整数中 1 出现的次数.js
+++ swordToOffer2/剑指 Offer 43. 1～n 整数中 1 出现的次数.js
@@ -5,14 +5,12 @@ function getCountOfN(n, prev) {
       return 1
     }
     const count = n - 1;
-    console.log(`count`, count);
     for (let i = count; i >= 0; i--) {
       if (i === count) {
         res += Math.pow(10, i)
       } else {
         res += 8 * Math.pow(10, i) * Math.pow(9, count - i - 1 >= 0 ? count - i - 1 : 0)
       }
-      console.log(`res`, res);
     }
   } else if (prev === '1') {
     res = Math.pow(10, n)
@@ -29,6 +27,28 @@ function getCountOfN(n, prev) {
  * @return {number}
  */
 var countDigitOne = function (n) {
+  const strN = `${n}`;
+  const len = strN.length
+  if (len == 1) {
+    return 1
+  }
+  const res = 0;
+  for (let i = 0; i < len; i++) {
+    let curNum = strN[i];
+    const count = len - i - 1;
+    if (count > 1) {
+      if (i === 0) {
+        for (let j = 0; j < curNum; j++) {
+
+        }
+      } else {
+
+      }
+    } else {
+
+    }
+
+  }
 
 };
 
@@ -42,8 +62,11 @@ function test() {
   const res = fun.apply(null, params)
   console.log(`res`, res);
   // console.log(getCountOfN(3));
+  // console.log(`getCountOfN(1,'0')`, getCountOfN(1, '0'));
+  // console.log(`getCountOfN(2,'0')`, getCountOfN(2, '0'));
   // console.log(`getCountOfN(2,'0')`, getCountOfN(2, '0'));
   // console.log(`getCountOfN(3,'0')`, getCountOfN(3, '0'));
-  console.log(`getCountOfN(4,'0')`, getCountOfN(4, '0'));
+  // console.log(`getCountOfN(4,'0')`, getCountOfN(4, '0'));
+  // console.log(`getCountOfN(5,'0')`, getCountOfN(5, '0'));
 }
 test()
