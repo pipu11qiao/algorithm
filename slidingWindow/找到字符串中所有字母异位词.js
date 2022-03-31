a2ee392ed6d2f70b4e9181167598829eba1de60c
王勇 <wangyong16@xiaomi.com>
2022-03-31 18:46:00 +0800

    feat: update

--- slidingWindow/找到字符串中所有字母异位词.js
+++ slidingWindow/找到字符串中所有字母异位词.js
@@ -4,13 +4,58 @@
  * @return {number[]}
  */
 var findAnagrams = function (s, p) {
+  let pMap = {};
+  let countP = 0
+  for (let i = 0; i < p.length; i++) {
+    const curLetter = p[i]
+    if (missMap[curLetter] === undefined) {
+      pMap[curLetter] = 0;
+    }
+    pMap[curLetter]++
+    countP++
+  }
+  let missMap = { ...pMap };
+  let count = 0;
+  let misCount = countP;
+  let end = 0;
+  let resArr = [];
+  let curMap = {};
+  for (let i = 0; i < s.length; i++) {
+    const curLetter = s[i]
+    count++
+    if (curMap[curLetter] === undefined) {
+      curMap[curLetter] = 0;
+    }
+    if (count < countP) {
+      if (pMap[curLetter]) {
+        if (missMap[curLetter] > 0) {
+          missMap[curLetter]--
+          misCount--
+        }
+      }
+      continue
+    }
+
+    curMap[curLetter]++;
+    if (pMap[curLetter]) {
+      if (missMap[curLetter] > 0) {
+        missMap[curLetter]--
+        misCount--
+      }
+      if (misCount === 0) {
+        resArr.push(end);
+      }
+    }
+    // 右移一位
+  }
 
 };
 
 function test() {
   let fun = findAnagrams;
+
   let params = [
-    [1, 1, 1, 0]
+    "cbaebabacd", "abc"
   ];
 
 
